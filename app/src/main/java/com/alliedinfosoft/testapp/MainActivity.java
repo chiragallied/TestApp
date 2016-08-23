45dbb7d70fd2066f01177cfdf91a0307a96e2cd8
chiragallied <chirag.allied@gmail.com>
2016-08-23 14:28:38 +0530

    New Change

--- app/src/main/java/com/alliedinfosoft/testapp/MainActivity.java
+++ app/src/main/java/com/alliedinfosoft/testapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_LONG).show();
+        int a;
     }
 }
